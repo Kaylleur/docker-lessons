7e682b2aae808009181648110c8fa8507174b4d0
Thomas Brunet <contact@thomas-brunet.fr>
2023-10-19 09:00:22 +0200

    new tp

--- 06_deployments/docker_env/app.js
+++ 06_deployments/docker_env/app.js
@@ -3,13 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const res = Object.keys(process.env).filter((key) => key.startsWith('MY_')).reduce((obj, key) => {
+const envs = Object.keys(process.env).filter((key) => key.startsWith('MY_')).reduce((obj, key) => {
   obj[key] = process.env[key];
   return obj;
 }, {});
 
 app.get('/', (req, res) => {
-  res.json(res);
+  res.json(envs);
 });
 
 app.listen(PORT, '0.0.0.0', () => {
